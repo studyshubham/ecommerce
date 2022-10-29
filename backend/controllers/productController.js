46b6601a568bf79760fa55a3ceb1d617ecae9c79
studyshubham <studyshubham007@gmail.com>
2022-10-29 19:18:12 +0530

    Update Product API

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -22,3 +22,26 @@ exports.getAllProducts = async (req,res) => {
         products    
     })
 }
+
+// Updated Product -- Admin
+exports.updateProduct = async (req,res,next) => {
+    let product = await Product.findById(req.params.id);
+
+    if(!product){
+        return res.status(500).json({
+            success:false,
+            message:"Product not found"
+        })
+    }
+
+    product = await Product.findByIdAndUpdate(req.params.id,req.body,{
+        new:true,
+        runValidators:true,
+        useFindAndModify:false
+    });
+
+    res.status(200).json({
+        success:true,
+        product
+    })
+}
