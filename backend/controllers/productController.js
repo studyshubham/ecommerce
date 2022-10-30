468ceb23bccec193db4097137f25a83e81ae49eb
studyshubham <studyshubham007@gmail.com>
2022-10-30 21:41:20 +0530

    Created Get Single Product API

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -23,6 +23,23 @@ exports.getAllProducts = async (req,res) => {
     })
 }
 
+// Get Product Details
+exports.getProductDetails = async (req,res,next) => {
+    const product = await Product.findById(req.params.id);
+
+    if(!product){
+        return res.status(500).json({
+            success:false,
+            message:"Product not found"
+        })
+    }
+    
+    res.status(200).json({
+        success:true,
+        product
+    })
+}
+
 // Updated Product -- Admin
 exports.updateProduct = async (req,res,next) => {
     let product = await Product.findById(req.params.id);
