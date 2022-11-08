dca9c9cb284d8c174d69d2b2e039975ed2f25bc7
studyshubham <studyshubham007@gmail.com>
2022-11-08 21:55:13 +0530

    Added Error Middleware

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -1,4 +1,5 @@
 const Product = require("../models/productModel");
+const ErrorHandler = require("../utils/errorhandler");
 
 
 
@@ -28,10 +29,7 @@ exports.getProductDetails = async (req,res,next) => {
     const product = await Product.findById(req.params.id);
 
     if(!product){
-        return res.status(500).json({
-            success:false,
-            message:"Product not found"
-        })
+        return next(new ErrorHandler("Product not found",404));
     }
     
     res.status(200).json({
