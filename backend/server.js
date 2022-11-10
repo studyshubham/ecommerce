2d92787ed99182e635b2e0f5d670ec3f793f64b2
studyshubham <studyshubham007@gmail.com>
2022-11-10 10:54:16 +0530

    Handling uncaught Exeption, Unhandled Promise Rejection, Added catchAsyncError middleware and used it in Product Controller , Added Wrong Mongo Id error Handler

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,13 @@ const app = require("./app");
 const dotenv = require("dotenv");
 const connectDatabase = require("./config/database");
 
+// ######### handling uncaught Exception #############
+process.on("uncaughtException",(err)=>{
+    console.log(`Error: ${err.message}`);
+    console.log(`sutting down the server due to handling uncaught Exception `)  
+    process.exit(1);
+});
+
 // Config
 
 dotenv.config({path:"backend/config/config.env"});
@@ -10,6 +17,16 @@ dotenv.config({path:"backend/config/config.env"});
 //Connecting to Database
 connectDatabase();
 
-app.listen(process.env.PORT,() =>{
+const server = app.listen(process.env.PORT,() =>{
     console.log(`Server is working on http://localhost:${process.env.PORT}`)
 })
+
+// ########### unhandled promise Rejaction ###########
+
+process.on("unhandledRejection",err=>{
+    console.log(`Error: ${err.message}`);
+    console.log(`sutting down the server due to unhandled promise Rejaction `)
+    server.close(()=>{
+        process.exit(1)
+    });
+});
